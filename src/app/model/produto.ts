1ab7b9ffde78f9d3a9ce2a94606a984bcd39dfc3
Lucas Reis <111098018+luks-reiis@users.noreply.github.com>
2024-04-21 21:57:46 -0300

    Finalizado Vitrine

---
+++ src/app/model/produto.ts
@@ -0,0 +1,21 @@
+export class Produto {
+    public codigo: number;
+    public nome: string;
+    public descritivo: string;
+    public imagem: string;
+    public valor: number;
+    public valorPromo: number;
+    public destaque: number;
+    public estoque: number;
+
+    constructor(){
+        this.codigo = 0;
+        this.nome = "";
+        this.descritivo = "";
+        this.imagem = "";
+        this.valor = 0;
+        this.valorPromo = 0;
+        this.destaque = 0;
+        this.estoque = 0;
+    }
+}
--- src/app/vitrine/vitrine.component.ts
+++ src/app/vitrine/vitrine.component.ts
@@ -1,13 +1,96 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { Produto } from '../model/produto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vitrine',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './vitrine.component.html',
   styleUrl: './vitrine.component.css'
 })
 export class VitrineComponent {
-
+  public listaProdutos: Produto[] = [
+    {
+      codigo: 1,
+      nome: "Nintendo 64",
+      descritivo: "Console Nintendo 64 Original (Seminovo). Acompanha: 2 controles; fonte de alimentação; cabo AV; 1 Controller Pak",
+      imagem: "n64.png",
+      valor: 430.00,
+      valorPromo: 350.00,
+      destaque: 1,
+      estoque: 10
+    },
+    {
+      codigo: 2,
+      nome: "Mega Drive",
+      descritivo: "Console Sega Mega Drive II Original (Seminovo). Acompanha: 2 controles; fonte de alimentação; cabo AV",
+      imagem: "megadrive.png",
+      valor: 350.00,
+      valorPromo: 300.00,
+      destaque: 1,
+      estoque: 10
+    },
+    {
+      codigo: 3,
+      nome: "Game Boy Color",
+      descritivo: "Portátil Game Boy Color Amarelo Original (Seminovo). Acompanha: 2 pilhas AA",
+      imagem: "gameboycolor.png",
+      valor: 650.00,
+      valorPromo: 620.00,
+      destaque: 1,
+      estoque: 1
+    },
+    {
+      codigo: 4,
+      nome: "Game Gear",
+      descritivo: "Portátil Sega Game Gear Original (Seminovo). Acompanha: 4 pilhas AA",
+      imagem: "Sega-Game-Gear-WB.png",
+      valor: 1500.00,
+      valorPromo: 1220.00,
+      destaque: 1,
+      estoque: 1
+    },
+    {
+      codigo: 5,
+      nome: "Cartucho Sonic 3",
+      descritivo: "Cartucho Sonic The Hedgehog 3 reprodução para o console Sega Mega Drive",
+      imagem: "sonic3.png",
+      valor: 100.00,
+      valorPromo: 60.00,
+      destaque: 1,
+      estoque: 13
+    },
+    {
+      codigo: 6,
+      nome: "Cartucho Super Mario 64",
+      descritivo: "Cartucho Super Mario 64 reprodução para o console Nintendo 64",
+      imagem: "mario64.png",
+      valor: 150.00,
+      valorPromo: 100.00,
+      destaque: 1,
+      estoque: 13
+    },
+    {
+      codigo: 7,
+      nome: "Tv de tubo LG",
+      descritivo: "Televisão de tubo LG, modelo 29FS4RL 29 polegadas",
+      imagem: "tvtubolg.png",
+      valor: 200.00,
+      valorPromo: 100.00,
+      destaque: 1,
+      estoque: 1
+    },
+    {
+      codigo: 8,
+      nome: "Tv de tubo Semp Toshiba",
+      descritivo: "Televisão de tubo Semp Toshiba, modelo 1452AV 14 polegadas",
+      imagem: "tvtubotoshiba.png",
+      valor: 150.00,
+      valorPromo: 100.00,
+      destaque: 1,
+      estoque: 1
+    }
+  ]
 }
